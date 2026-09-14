cb447fec6b8ce55a7cc774354cb96877ba2247d6
TjThymes <thijmenroozendaal1@gmail.com>
2026-09-14 10:57:21 +0200

    joooo

--- Room.h
+++ Room.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <memory>
 class Room
 {
 	std::string name;
 	std::string description;
-	bool exit;
+	std::shared_ptr<Room> exits;
 	std::vector<std::string> items;
 	
 	void showinfo() const;
