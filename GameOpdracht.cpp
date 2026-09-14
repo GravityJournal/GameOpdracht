692470bfc620d44c899148127381534d3264d61b
TOKAT60671 <tomasramerman@gmail.com>
2026-09-14 11:22:39 +0200

    Update GameOpdracht.cpp

--- GameOpdracht.cpp
+++ GameOpdracht.cpp
@@ -12,30 +12,17 @@ int main()
     auto startKamer = std::make_shared<Room>("Start Kamer", "Dit is een lege kamer met een stoel. Er lopen overal ratten. er is een deur.");
     auto raadselKamer = std::make_shared<Room>("RaadselKamer", "JOOOOOO");
 	auto codeKamer = std::make_shared<Room>("CodeKamer", "CODEEEEE");
+	auto slotKamer = std::make_shared<Room>("SlotKamer", "Dit is een Kamer met een grote deur, in deze deur zitten 3 sleutelgaten");
 
 
     startKamer->exits["North"] = raadselKamer;
     startKamer->exits["East"] = codeKamer;
+    startKamer->exits["West"] = slotKamer;
 
 
 	raadselKamer->exits["South"] = startKamer;
 	codeKamer->exits["West"] = startKamer;
+    slotKamer->exits["East"] = startKamer;
 
 
-
-
-
-    
-    
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
